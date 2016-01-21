0f5bba5db74219e134b0a8dc5adc20973756ec3d
abhishek <bastisanu@gmail.com>
2016-01-21 16:25:35 +0530

    student controller changes

--- WebApplication1/SchoolManagmentApp/Controllers/StudentController.cs
+++ WebApplication1/SchoolManagmentApp/Controllers/StudentController.cs
@@ -3,15 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using DLLApp.ViewModels;
 
 namespace SchoolManagmentApp.Controllers
 {
-    public class StudentController : Controller
+    public class StudentController : BaseController
     {
         // GET: Student
-        public ActionResult AddStudent()
+        [HttpGet]
+        public ActionResult AddStudent(int? id)
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddStudent(StudentInfoViewModels models)
+        {
+            return View(models);
         }
     }
 }
