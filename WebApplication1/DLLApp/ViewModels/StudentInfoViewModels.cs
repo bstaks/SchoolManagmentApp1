ade0669e2fad4c9b468e63f22f0286d0a2b29e0d
abhishek <bastisanu@gmail.com>
2016-01-21 15:18:59 +0530

    change in projects

---
+++ WebApplication1/DLLApp/ViewModels/StudentInfoViewModels.cs
@@ -0,0 +1,16 @@
+﻿using DLLApp.DataModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DLLApp.ViewModels
+{
+   public class StudentInfoViewModels
+    {
+        public Student Student { get; set; }
+        public AspNetUser Users { get; set; }
+        public List<AddressInfo> AddressInfo { get; set; }
+    }
+}
